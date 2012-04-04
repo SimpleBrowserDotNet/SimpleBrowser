5ec02759009516073fac001a714de773dd67d421
Teun Duynstee <github@duynstee.com>
2012-04-04 10:45:17 +0200

    Created a separate unit test library using NUnit. Tests that were present in the main library have been moved here and made non-network dependent. Needs work, as URL used in old tests gives 404.

---
+++ SimpleBrowser.UnitTests/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace SimpleBrowser.UnitTests
+{
+	public class Helper
+	{
+		internal static string GetFromResources(string resourceName)
+		{
+			Assembly assem = Assembly.GetExecutingAssembly();
+			using (Stream stream = assem.GetManifestResourceStream(resourceName))
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					return reader.ReadToEnd();
+				}
+			}
+		}
+
+	}
+}
