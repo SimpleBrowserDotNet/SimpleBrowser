af9e917529586a96d922d89ded9117abead370fa
Teun Duynstee <github@duynstee.com>
2012-05-16 23:36:38 +0200

    Stubbed away HttpwebRequest and HttpWebResponse, so I can make unit tests doing requests to real URLs without actually requesting them. Sample unit test mocks the behavior of the MVC Sample App 'Movies'.

--- SimpleBrowser.UnitTests/Helper.cs
+++ SimpleBrowser.UnitTests/Helper.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.IO;
+using SimpleBrowser.Network;
+using Moq;
+using System.Net;
 
 namespace SimpleBrowser.UnitTests
 {
@@ -19,6 +22,52 @@ namespace SimpleBrowser.UnitTests
 					return reader.ReadToEnd();
 				}
 			}
+		}
+		internal static IWebRequestFactory GetMoviesRequestMocker()
+		{
+			return new MoviesRequestMocker();
+		}
+		class MoviesRequestMocker : IWebRequestFactory
+		{
+
+			#region IWebRequestFactory Members
+
+			public IHttpWebRequest GetWebRequest(Uri url)
+			{
+				var mock = new Mock<IHttpWebRequest>();
+				mock.SetupAllProperties();
+				mock.Setup(m => m.GetResponse())
+					.Returns(() =>
+					{
+						var mockResponse = new Mock<IHttpWebResponse>();
+						mockResponse.SetupAllProperties();
+						mockResponse.SetupProperty(m => m.Headers, new WebHeaderCollection());
+
+						byte[] responseContent = new byte[0];
+						if (mock.Object.Method == "GET")
+						{
+							if (url.AbsolutePath == "/movies/")
+								responseContent = Encoding.UTF8.GetBytes(GetFromResources("SimpleBrowser.UnitTests.SampleDocs.movies1.htm"));
+							if (url.AbsolutePath == "/movies/Movies/Create")
+								responseContent = Encoding.UTF8.GetBytes(GetFromResources("SimpleBrowser.UnitTests.SampleDocs.movies2.htm"));
+						}
+						else if (mock.Object.Method == "POST")
+						{
+							if (url.AbsolutePath == "/movies/Movies/Create")
+							{
+								mockResponse.Object.StatusCode = HttpStatusCode.Moved;
+								mockResponse.Object.Headers.Add(HttpResponseHeader.Location, "http://localhost/movies/");
+							}
+						}
+						mockResponse.Setup(r => r.GetResponseStream()).Returns(new MemoryStream(responseContent));
+						return mockResponse.Object;
+					});
+				mock.SetupProperty(m => m.Headers, new WebHeaderCollection());
+				mock.Setup(m => m.GetRequestStream()).Returns(new MemoryStream(new byte[20000]));
+				return mock.Object;
+			}
+
+			#endregion
 		}
 
 	}
---
+++ SimpleBrowser/Network/WebRequestWrapper.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace SimpleBrowser.Network
+{
+	class WebRequestWrapper : IHttpWebRequest
+	{
+		public WebRequestWrapper(Uri url)
+		{
+			_wr = (HttpWebRequest)HttpWebRequest.Create(url);
+		}
+		HttpWebRequest _wr = null;
+
+		#region IHttpWebRequest Members
+
+		public System.IO.Stream GetRequestStream()
+		{
+			return _wr.GetRequestStream();
+		}
+
+		public IHttpWebResponse GetResponse()
+		{
+			return new WebResponseWrapper((HttpWebResponse)_wr.GetResponse());
+		}
+
+		public long ContentLength
+		{
+			get
+			{
+				return _wr.ContentLength;
+			}
+			set
+			{
+				_wr.ContentLength = value;
+			}
+		}
+
+		public WebHeaderCollection Headers
+		{
+			get
+			{
+				return _wr.Headers;
+			}
+			set
+			{
+				_wr.Headers = value;
+			}
+		}
+
+		public string ContentType
+		{
+			get
+			{
+				return _wr.ContentType;
+			}
+			set
+			{
+				_wr.ContentType = value;
+			}
+		}
+
+		public string Method
+		{
+			get
+			{
+				return _wr.Method;
+			}
+			set
+			{
+				_wr.Method = value;
+			}
+		}
+
+		public string UserAgent
+		{
+			get
+			{
+				return _wr.UserAgent;
+			}
+			set
+			{
+				_wr.UserAgent = value;
+			}
+		}
+
+		public string Accept
+		{
+			get
+			{
+				return _wr.Accept;
+			}
+			set
+			{
+				_wr.Accept = value; ;
+			}
+		}
+
+		public int Timeout
+		{
+			get
+			{
+				return _wr.Timeout;
+			}
+			set
+			{
+				_wr.Timeout = value;
+			}
+		}
+
+		public bool AllowAutoRedirect
+		{
+			get
+			{
+				return _wr.AllowAutoRedirect;
+			}
+			set
+			{
+				_wr.AllowAutoRedirect = value;
+			}
+		}
+
+		public CookieContainer CookieContainer
+		{
+			get
+			{
+				return _wr.CookieContainer;
+			}
+			set
+			{
+				_wr.CookieContainer = value;
+			}
+		}
+
+		public IWebProxy Proxy
+		{
+			get
+			{
+				return _wr.Proxy;
+			}
+			set
+			{
+				_wr.Proxy = value;
+			}
+		}
+
+		public string Referer
+		{
+			get
+			{
+				return _wr.Referer;
+			}
+			set
+			{
+				_wr.Referer = value;
+			}
+		}
+
+		#endregion
+	}
+}
