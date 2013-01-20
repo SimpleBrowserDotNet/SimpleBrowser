963f869b6999803bf8dd28a6561799e312051870
Joe Feser <joseph.feser@gmail.com>
2013-01-20 14:58:57 -0500

    Add GZip unit tests

---
+++ SimpleBrowser.UnitTests/OnlineTests/VerifyGZipEncoding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SimpleBrowser.UnitTests.OnlineTests
+{
+    [TestFixture]
+    public class VerifyGZipEncoding
+    {
+
+        [Test]
+        public void When_Setting_GZip_Encoding_Content_Should_Still_Be_Returned_As_Text()
+        {
+            Browser b = new Browser();
+            b.UseGZip = true;
+            HttpRequestLog lastRequest = null;
+            b.RequestLogged += (br, l) =>
+            {
+                lastRequest = l;
+            };
+            b.Navigate("http://www.google.com/");
+            Assert.That(b.Url == new Uri("http://www.google.com/"));
+            Assert.That(b.Select("Title") != null);
+            Assert.That(b.Select("Title").Value.IndexOf("Google", StringComparison.OrdinalIgnoreCase) > -1);
+        }
+
+    }
+}
