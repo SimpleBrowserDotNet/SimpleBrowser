20171d175343ffbd224b5657bd33374847af7a3e
Kevin Yochum <kevingy@yahoo.com>
2013-10-21 14:26:07 -0400

    Code clean up (eliminate build warnings, StyleCop, Code Anaysis issues)

--- SimpleBrowser/Parser/DocumentCleaner.cs
+++ SimpleBrowser/Parser/DocumentCleaner.cs
@@ -1,53 +1,29 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
+﻿// -----------------------------------------------------------------------
+// <copyright file="DocumentCleaner.cs" company="SimpleBrowser">
+// See https://github.com/axefrog/SimpleBrowser/blob/master/readme.md
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace SimpleBrowser.Parser
 {
-	public static class DocumentCleaner
-	{
-		public static void Rebuild(XDocument doc)
-		{
-			if(string.Compare(doc.Root.Name.LocalName, "html", true) != 0)
-			{
-				var root = new XElement("html");
-				doc.Root.ReplaceWith(root);
-			}
-			return;
+    using System.Xml.Linq;
 
-			foreach(var item in (from e in doc.Descendants()
-									let n = new { Element = e, Ancestors = e.Ancestors() }
-									orderby n.Ancestors.Count() descending
-									select n))
-			{
-				if(item.Element.Parent == null)
-					continue;
-				var rule = ElementPositioningRule.Get(item.Element.Name.LocalName.ToLowerInvariant());
-				if(rule == null) continue;
-
-				// restrict children according to whether or not they are text nodes
-				if(rule.TextChildren.HasValue)
-				{
-					var textNodes = (IEnumerable<XText>)item.Element.Nodes().Where(n => n is XText);
-					if(rule.TextChildren.Value)
-					{
-						var sb = new StringBuilder();
-						foreach(var node in textNodes)
-							sb.Append(node.Value);
-						item.Element.RemoveNodes();
-						item.Element.Add(new XText(sb.ToString()));
-					}
-					else
-					{
-						foreach(var node in textNodes)
-							node.Remove();
-					}
-				}
-
-				//DocumentArea area
-				//rule.Area
-			}
-		}
-	}
+    /// <summary>
+    /// Implements a class to clean up HTML
+    /// </summary>
+    public static class DocumentCleaner
+    {
+        /// <summary>
+        /// Rebuilds the document
+        /// </summary>
+        /// <param name="doc">The document to clean</param>
+        public static void Rebuild(XDocument doc)
+        {
+            if (string.Compare(doc.Root.Name.LocalName, "html", true) != 0)
+            {
+                var root = new XElement("html");
+                doc.Root.ReplaceWith(root);
+            }
+        }
+    }
 }
--- SimpleBrowser/Query/XQueryException.cs
+++ SimpleBrowser/Query/XQueryException.cs
@@ -1,23 +1,74 @@
-﻿using System;
+﻿// -----------------------------------------------------------------------
+// <copyright file="XQueryException.cs" company="SimpleBrowser">
+// See https://github.com/axefrog/SimpleBrowser/blob/master/readme.md
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace SimpleBrowser.Query
 {
-	public class XQueryException : Exception
-	{
-		public string Query { get; set; }
-		public int Index { get; set; }
-		public int Length { get; set; }
+    using System;
+    using System.Runtime.Serialization;
+    using System.Security.Permissions;
 
-		public XQueryException(string message, string query, int index, int length)
-			: base(message)
-		{
-			Query = query;
-			if(index >= query.Length)
-				index = query.Length - 1;
-			Index = index;
-			if(index + length >= query.Length)
-				length = query.Length - index;
-			Length = length;
-		}
-	}
+    /// <summary>
+    /// Implements an XQuery exception
+    /// </summary>
+    [Serializable]
+    public class XQueryException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XQueryException"/> class.
+        /// </summary>
+        /// <param name="message">A descriptive error message</param>
+        /// <param name="query">The query causing the exception to be thrown</param>
+        /// <param name="index">The location of the error in the query</param>
+        /// <param name="length">The length of the query</param>
+        public XQueryException(string message, string query, int index, int length)
+            : base(message)
+        {
+            this.Query = query;
+            if (index >= query.Length)
+            {
+                index = query.Length - 1;
+            }
+
+            this.Index = index;
+            if (index + length >= query.Length)
+            {
+                length = query.Length - index;
+            }
+
+            this.Length = length;
+        }
+
+        /// <summary>
+        /// Gets or sets the query causing the exception to be thrown
+        /// </summary>
+        public string Query { get; set; }
+
+        /// <summary>
+        /// Gets or sets the index of the error in the query
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Gets or sets the length of the query
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// Populates a SerializationInfo with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">A <see cref="SerializationInfo"/> to populate/></param>
+        /// <param name="context">The <see cref="StreamingContext"/> of the <see cref="SerializationInfo"/></param>
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue("Query", this.Query);
+            info.AddValue("Index", this.Index);
+            info.AddValue("Length", this.Length);
+        }
+    }
 }
