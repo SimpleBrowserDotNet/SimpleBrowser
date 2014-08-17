590404d981930236b863cacd6ae65606f5ae2a29
Anders Lindén <anderslinden@bredband.net>
2014-08-17 09:45:51 +0200

    Browser.CurrentHtml setter.

---
+++ SimpleBrowser.UnitTests/OfflineTests/CurrentHtml.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace SimpleBrowser.UnitTests.OfflineTests
+{
+	[TestFixture]
+	public class CurrentHtml
+	{
+		[Test]
+		public void CurrentHtml_Setter()
+		{
+			Browser b = new Browser();
+
+			b.CurrentHtml = "<div>test</div>";
+			HtmlResult div = b.Select("div");
+
+			Assert.That(div.TotalElementsFound, Is.EqualTo(1));
+
+			Assert.That(b.NavigateBack(), Is.False);
+		}
+	}
+}
