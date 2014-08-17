5e33176cc1cbd1ba881133a5f4c1231377f3f90b
Teun Duynstee <github@duynstee.com>
2014-08-17 16:59:33 +0200

    Added unit test to file-url pull request

---
+++ SimpleBrowser.UnitTests/OfflineTests/FileUri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace SimpleBrowser.UnitTests.OfflineTests
+{
+	[TestFixture]
+	public class FileUri
+	{
+		[Test]
+		public void CanLoadHtmlFromFile()
+		{
+			Regex start = new Regex("^([a-z]):\\\\");
+			var b = new Browser();
+			var f = new FileInfo(".\\SampleDocs\\movies1.htm");
+			string uri = start.Replace(f.FullName, "file:///$1/");
+			uri = uri.Replace("\\", "/");
+			b.Navigate(uri);
+			Assert.AreEqual(b.Select("ul#menu>li").Count(), 3, "Not loaded");
+		}
+	}
+}
