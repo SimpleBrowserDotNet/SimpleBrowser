627bb1ac043a0b31b2d9efeca732ed37b81761d8
Anders Lindén <AndersBillLinden@users.noreply.github.com>
2014-09-21 13:21:49 +0200

    Keeping the request factory in copies of Browser.

--- SimpleBrowser/Browser.cs
+++ SimpleBrowser/Browser.cs
@@ -244,7 +244,7 @@ namespace SimpleBrowser
 
 		public Browser CreateReferenceView()
 		{
-			Browser b = new Browser
+			Browser b = new Browser(_reqFactory)
 			{
 				Cookies = Cookies,
 				_doc = _doc,
